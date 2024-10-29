779ee1f64d83ce740f902ac93770f1f1fe2941c1
Im-Os <114974415+Im-Os@users.noreply.github.com>
2024-10-28 23:30:10 -0600

    Delete PROYECTOUNIDAD2 directory

--- PROYECTOUNIDAD2/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import menu.menuLogin;
-
-public class Main {
-        public static void main(String[] args) {
-                menuLogin menu = new menuLogin();
-                menu.mostrarMenuPrincipal();
-        }
-}
